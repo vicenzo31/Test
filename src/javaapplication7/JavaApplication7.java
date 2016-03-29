44b604a9a790c281e14c20493f3cf3c269a498b8
Vicenzo <Vicenzo@130.120.112.21>
2016-03-29 19:37:37 +0200

    TEST1

--- src/javaapplication7/JavaApplication7.java
+++ src/javaapplication7/JavaApplication7.java
@@ -17,6 +17,7 @@ public class JavaApplication7 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("TEST");
+        System.out.println("TEST1");
     }
     
 }
